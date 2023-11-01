f53f31ed762552a8abf166ea2eed341235defe7b
NinDevs <ninmacdev@gmail.com>
2023-11-01 15:07:14 +0700

    flutter images

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,9 +28,17 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("App Title"),
+        title: const Text("Images Widget"),
       ),
-      body: const Center(
+      body: const Column(
+        children: [
+          Image(
+            image: AssetImage('assets/images/natural-wallpaper.jpg'),
+          ),
+          Image(
+            image: NetworkImage('https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJhbHxlbnwwfHwwfHx8MA%3D%3D'),
+          ),
+        ],
       ),
     );
   }
