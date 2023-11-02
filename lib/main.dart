b17af2d75a756b2bc75d2b8d0aa44f743b6e9d05
NinDevs <ninmacdev@gmail.com>
2023-11-02 13:39:32 +0700

    Card Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,9 +28,75 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("App Title"),
+        title: const Text("Card Widget"),
       ),
-      body: const Center(
+      body: Center(
+        child: Column(
+          children: [
+            Card(
+              // elevation: 20.0, //shadow card
+              // shadowColor: Colors.yellowAccent, // custom color shadow
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const ListTile(
+                    leading: Icon(Icons.album),
+                    title: Text("The Enchanted Nightingale"),
+                    subtitle:
+                        Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('BUY TICKETS'),
+                      ),
+                      const SizedBox(
+                        width: 5.0,
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('LISTEN'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Card(
+              // elevation: 20.0, //shadow card
+              // shadowColor: Colors.yellowAccent, // custom color shadow
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const ListTile(
+                    leading: Icon(Icons.album),
+                    title: Text("The Enchanted Nightingale"),
+                    subtitle:
+                    Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('BUY TICKETS'),
+                      ),
+                      const SizedBox(
+                        width: 5.0,
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('LISTEN'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
